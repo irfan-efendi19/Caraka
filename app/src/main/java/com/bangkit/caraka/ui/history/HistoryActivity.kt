ed2e6b591ef011cc75e156018fec219fb33be358
Mochammad Irfan Efendi <m.irfan.efendi19@gmail.com>
2023-12-05 20:12:40 +0700

    Layout HistoryActivity

---
+++ app/src/main/java/com/bangkit/caraka/ui/history/HistoryActivity.kt
@@ -0,0 +1,12 @@
+package com.bangkit.caraka.ui.history
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bangkit.caraka.R
+
+class HistoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history)
+    }
+}
