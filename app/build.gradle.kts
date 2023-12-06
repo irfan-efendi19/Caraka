aca4da8c1ca30ecb5169320b4bd4080be9bcfda5
M DOLYANDA HARIALDY <yayangharialdyy@gmail.com>
2023-12-06 17:07:48 +0700

    fixing error in gradle - androidx.animation:animation-ktx:1.12.0 - androidx.test.espresso:espresso-animation:3.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,8 @@ android {
 
 dependencies {
 
-    implementation("androidx.animation:animation-ktx:1.12.0")
+    implementation("androidx.core:core-animation:1.0.0-rc01")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -48,14 +49,14 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-animation:3.5.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     //navigation
     implementation ("androidx.navigation:navigation-fragment-ktx:2.7.5")
     implementation ("androidx.navigation:navigation-ui-ktx:2.7.5")
 
     //circle imageview
-    implementation ("de.hdodenhof:circleimageview:3.1.0")
+    implementation ("de.hdodenhof:circleimageview:3.1.0" )
 
     //viewPager2 library
     implementation("androidx.viewpager2:viewpager2:1.1.0-beta02")
