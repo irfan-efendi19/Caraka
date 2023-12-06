55b36b69307c1d227932c6ba5323c7d643e3b085
Mochammad Irfan Efendi <m.irfan.efendi19@gmail.com>
2023-12-07 01:27:42 +0700

    Layout ScoreActivity

---
+++ app/src/main/java/com/bangkit/caraka/ui/score/ScoreActivity.kt
@@ -0,0 +1,12 @@
+package com.bangkit.caraka.ui.score
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bangkit.caraka.R
+
+class ScoreActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_score)
+    }
+}
