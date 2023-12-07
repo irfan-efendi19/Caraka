cf5a59b4c1b17ca81535c59434609ec1dd1f569f
Mochammad Irfan Efendi <m.irfan.efendi19@gmail.com>
2023-12-08 05:22:11 +0700

    add intent & fix activity

---
+++ app/src/main/java/com/bangkit/caraka/ui/quiz/QuizActivity.kt
@@ -0,0 +1,12 @@
+package com.bangkit.caraka.ui.quiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bangkit.caraka.R
+
+class QuizActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz)
+    }
+}
